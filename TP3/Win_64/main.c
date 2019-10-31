7c70b9a086c54438715b91cbba9c40fc645742ea
Franco52001 <52410416+Franco52001@users.noreply.github.com>
2019-10-31 12:12:43 -0300

    Add files via upload

---
+++ TP3/Win_64/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.bi (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+
+
+int main()
+{
+    FILE* pFile;
+    Employee* empleadoA[1000];
+    LinkedList* listaEmpleados = ll_newLinkedList();
+    int i;
+
+    pFile = fopen("data.csv","r");
+
+    parser_EmployeeFromText(pFile,listaEmpleados);
+
+    for(i = 0;i < 1000; i++)
+        {
+
+        }
+
+
+
+    /*
+    int option = 0;
+    do{
+        switch(option)
+        {
+            case 1:
+                controller_loadFromText("data.csv",listaEmpleados);
+                break;
+        }
+    }while(option != 10);*/
+    return 0;
+}
